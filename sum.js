41d2f8a68892be0bd1e0d53bc395c9bdddea57b1
Simon Hoyos <shmesa23@gmail.com>
2021-05-26 10:15:29 -0500

    Solve merge conflicts

--- sum.js
+++ sum.js
@@ -2,10 +2,14 @@ function sum(a,b) {
   return a + b
 }
 
+function sub(a,b) {
+  return b - a
+}
+
 function multiply(a,b) {
   return a * b
 }
 
-function sub(a,b) {
-  return b - a
+function div(a,b) {
+  return a / b
 }
